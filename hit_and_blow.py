36b8c7d635dd01dd2a5e6da3bdee8d2f54f6c86e
Paul Kim <ezikial142857@gmail.com>
2022-05-04 16:53:45 +0900

    1. 게임 반복할 수 있도록 수정

--- hit_and_blow.py
+++ hit_and_blow.py
@@ -3,65 +3,83 @@ from PyQt5.QtCore import Qt
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from origin import check_answer
-from origin import create_task
+from origin import check_answer as ca
+from origin import create_task as ct
 
 form_class = uic.loadUiType("./hit_and_blow.ui")[0]
 
-image_path = "./resource/image/"
-max_digit = 4
-max_number = 10
-answer_list = [0, 0, 0, 0]
-image_list = []
-task = create_task.create_task(max_number, max_digit)
-for i in range(0, max_number):
-    image_list.append(image_path + "spade_" + str((i + 1)) + ".png")
 
-
-class WindowClass(QMainWindow, form_class):
+class MainWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.current_cursor = 0
-        self.arrow_down.setPixmap(QtGui.QPixmap(image_path + "arrow_down.png"))
+        self.digit = 4
+        self.max_number = 10
+        self.try_count = 0
+        self.answer = []
+        self.task = []
+        self.cursor = 0
+        self.image_path = "./resource/image/"
+        self.image_list = []
+        for i in range(0, self.max_number):
+            self.image_list.append(self.image_path + "spade_" + str((i + 1)) + ".png")
+
+        self.arrow_down.setPixmap(QtGui.QPixmap(self.image_path + "arrow_down.png"))
         self.btn_try.clicked.connect(self.button_try_clicked)
-        self.render()
+        self.init_game()
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_W:
-            answer_list[self.current_cursor] = (answer_list[self.current_cursor] + 1) % max_number
+            self.answer[self.cursor] = (self.answer[self.cursor] + 1) % self.max_number
         elif e.key() == Qt.Key_A:
-            if self.current_cursor == 0:
-                self.current_cursor = max_digit - 1
+            if self.cursor == 0:
+                self.cursor = self.digit - 1
             else:
-                self.current_cursor -= 1
+                self.cursor -= 1
         elif e.key() == Qt.Key_S:
-            if answer_list[self.current_cursor] == 0:
-                answer_list[self.current_cursor] = max_number - 1
+            if self.answer[self.cursor] == 0:
+                self.answer[self.cursor] = self.max_number - 1
             else:
-                answer_list[self.current_cursor] -= 1
+                self.answer[self.cursor] -= 1
         elif e.key() == Qt.Key_D:
-            self.current_cursor = (self.current_cursor + 1) % max_digit
+            self.cursor = (self.cursor + 1) % self.digit
         self.render()
 
     def button_try_clicked(self):
-        result_dict = check_answer.check_answer(answer_list, task)
-        print(result_dict)
-        QMessageBox.about(self, 'Result', 'Hit: ' + str(result_dict['hit']) + ', Blow: ' + str(result_dict['blow']))
+        self.try_count -= 1
+        result_dict = ca.check_answer(self.answer, self.task)
+        if result_dict['hit'] == 4:
+            QMessageBox.about(self, 'Success!', "Success! Restart Game.")
+            self.init_game()
+        else:
+            QMessageBox.about(self, 'Result', f"Hit: {str(result_dict['hit'])}, Blow: {str(result_dict['blow'])}, Try "
+                                              f"Count: {self.try_count}")
+            if self.try_count == 0:
+                temp_task = [i + 1 for i in self.task]
+                QMessageBox.about(self, 'Game Over', f"Game Over. Goal was {temp_task}. Restart Game.")
+                self.init_game()
 
     def render(self):
-        self.digit_0.setPixmap(QtGui.QPixmap(image_list[answer_list[0]]))
-        self.digit_1.setPixmap(QtGui.QPixmap(image_list[answer_list[1]]))
-        self.digit_2.setPixmap(QtGui.QPixmap(image_list[answer_list[2]]))
-        self.digit_3.setPixmap(QtGui.QPixmap(image_list[answer_list[3]]))
-        self.arrow_down.move(110 + (200 * self.current_cursor), 20)
+        self.digit_0.setPixmap(QtGui.QPixmap(self.image_list[self.answer[0]]))
+        self.digit_1.setPixmap(QtGui.QPixmap(self.image_list[self.answer[1]]))
+        self.digit_2.setPixmap(QtGui.QPixmap(self.image_list[self.answer[2]]))
+        self.digit_3.setPixmap(QtGui.QPixmap(self.image_list[self.answer[3]]))
+        base_pos_x = 110
+        interval_pos = 200
+        base_pos_y = 20
+        self.arrow_down.move(base_pos_x + (interval_pos * self.cursor), base_pos_y)
+
+    def init_game(self):
+        self.task = ct.create_task(self.max_number, self.digit)
+        self.try_count = 8
+        self.answer = [i for i in range(self.digit)]
+        self.cursor = 0
+        self.render()
 
 
-# 본 파일에서 실행되었을 때 동작
-# 다른 파일에서 import하여 실행했을 때에는 동작하지 않음.
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = WindowClass()
-    myWindow.show()
+    main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec_())
