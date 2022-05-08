429ce0cb34950bfc9620d73c4ec9663b89a49042
Paul Kim <ezikial142857@gmail.com>
2022-05-08 22:00:36 +0900

    1. UI: 로그 추가 2. 로그 기능 추가 3. 조작 추가

--- hit_and_blow.py
+++ hit_and_blow.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 from PyQt5.QtCore import Qt
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
@@ -21,7 +22,6 @@ class MainWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.digit = 4
         self.max_number = 10
         self.try_count = 0
@@ -30,43 +30,57 @@ class MainWindow(QMainWindow, form_class):
         self.cursor = 0
         self.image_path = './resource/image/'
         self.image_list = []
+        self.log = ""
+        self.round = 0
         for i in range(0, self.max_number):
             self.image_list.append(self.image_path + "spade_" + str((i + 1)) + ".png")
-
         self.arrow_down.setPixmap(QtGui.QPixmap(resource_path(self.image_path + "arrow_down.png")))
         self.btn_try.clicked.connect(self.button_try_clicked)
         self.init_game()
 
     def keyPressEvent(self, e):
-        if e.key() == Qt.Key_W:
+        if e.key() == Qt.Key_W or e.key() == Qt.Key_Up or e.key() == Qt.Key_8:
             self.answer[self.cursor] = (self.answer[self.cursor] + 1) % self.max_number
-        elif e.key() == Qt.Key_A:
+        elif e.key() == Qt.Key_A or e.key() == Qt.Key_Left or e.key() == Qt.Key_4:
             if self.cursor == 0:
                 self.cursor = self.digit - 1
             else:
                 self.cursor -= 1
-        elif e.key() == Qt.Key_S:
+        elif e.key() == Qt.Key_S or e.key() == Qt.Key_Down or e.key() == Qt.Key_2:
             if self.answer[self.cursor] == 0:
                 self.answer[self.cursor] = self.max_number - 1
             else:
                 self.answer[self.cursor] -= 1
-        elif e.key() == Qt.Key_D:
+        elif e.key() == Qt.Key_D or e.key() == Qt.Key_Right or e.key() == Qt.Key_6:
             self.cursor = (self.cursor + 1) % self.digit
+        elif e.key() == Qt.Key_Return or e.key() == Qt.Key_Enter:
+            self.button_try_clicked()
         self.render()
+
+    def write_log(self, content, title=""):
+        if title == "":
+            self.log += f"{content}\n"
+        else:
+            QMessageBox.about(self, title, content)
+            self.log += f"[{title}] {content}\n"
 
     def button_try_clicked(self):
         self.try_count -= 1
         result_dict = ca.check_answer(self.answer, self.task)
-        if result_dict['hit'] == 4:
+        if result_dict['hit'] == self.digit:
             QMessageBox.about(self, 'Success!', "Success! Restart Game.")
             self.init_game()
+            return
         else:
-            QMessageBox.about(self, 'Result', f"Hit: {str(result_dict['hit'])}, Blow: {str(result_dict['blow'])}")
-            if self.try_count == 0:
+            temp_answer = [i + 1 for i in self.answer]
+            self.write_log(f"[round {self.round + 1}] {temp_answer}: {result_dict}", "Result")
+            if self.try_count <= 0:
                 temp_task = [i + 1 for i in self.task]
                 QMessageBox.about(self, 'Game Over', f"Game Over. Goal was {temp_task}. Restart Game.")
                 self.init_game()
+                return
         self.render()
+        self.round += 1
 
     def render(self):
         self.digit_0.setPixmap(QtGui.QPixmap(resource_path(self.image_list[self.answer[0]])))
@@ -77,6 +91,7 @@ class MainWindow(QMainWindow, form_class):
         interval_pos = 150
         base_pos_y = -10
         self.arrow_down.move(base_pos_x + (interval_pos * self.cursor), base_pos_y)
+        self.txt_log.setText(self.log)
         self.txt_try_count.setText(f"{self.try_count}")
         if self.try_count <= 3:
             self.txt_try_count.setStyleSheet("Color : red")
@@ -85,9 +100,11 @@ class MainWindow(QMainWindow, form_class):
 
     def init_game(self):
         self.task = ct.create_task(self.max_number, self.digit)
-        self.try_count = 8
+        self.try_count = 10
         self.answer = [i for i in range(self.digit)]
         self.cursor = 0
+        self.round = 0
+        self.log = ""
         self.render()
 
 
